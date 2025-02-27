854ffd2db8b1e1684c9db338de0a3ffc2203463e
divya-subramonian25 <divya.subramonian25@gmail.com>
2025-02-26 20:40:39 -0800

    ds - added journal textbox

--- app/screens/journal.tsx
+++ app/screens/journal.tsx
@@ -1,10 +1,19 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
+import React, {useState} from 'react'
 
 export default function profile() {
+  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Journal screen</Text>
+      <Text style={styles.text}>Journaling Screen</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        value={text}
+        placeholder="Enter your thoughts ₍ᐢ•ﻌ•ᐢ₎"
+      />
     </View>
+    
   );
 }
 
@@ -16,6 +25,16 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },
     text: {
-      color: '#a66d45',
+      color: '#ad5775',
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    input: {
+      height: 300,
+      width: 400,
+      borderColor: '#ad5775',
+      borderWidth: 3,
+      marginBottom: 10,
+      paddingHorizontal: 10,
     },
 });
